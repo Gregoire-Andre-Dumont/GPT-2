68f40215156c2bb8fd3f1823253e538cd67d86a6
gregoire <dumont.gregoire.c@gmail.com>
2025-02-25 14:23:12 +0100

    created the tokenizer

--- train.py
+++ train.py
@@ -1,14 +1,23 @@
 """train is the main script for training and fine-tuning GPT 2 for text8."""
 
-import os
 import hydra
-import warnings
+import pandas as pd
+from pathlib import Path
 from omegaconf import DictConfig
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="train")
 def run_train(cfg: DictConfig) -> None:
     """Train the GPT 2 model with a train-test split. Hydra loads the config file."""
+
+    # Load the training and validation texts
+    train = pd.read_parquet(cfg.train_path)['text']
+    validation = pd.read_parquet(cfg.val_path)['text']
+
+    # Check whether the tokenizer is already trained
+    tokenizer = hydra.utils.instantiate(cfg.tokenizer)
+    tokenizer.train(train[0][:100], 500)
+    a = 1
 
 
 
