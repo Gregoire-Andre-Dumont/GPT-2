fda1ae0fda72697cbf0e4db9adbcfe54def5b908
gregoire <dumont.gregoire.c@gmail.com>
2025-03-01 11:27:08 +0100

    Implemented torch seed

---
+++ src/setup/setup_seed.py
@@ -0,0 +1,17 @@
+"""Set seed for reproducibility."""
+import torch
+
+def set_torch_seed(seed: int = 42) -> None:
+    """Set torch seed for reproducibility.
+
+    :param seed: seed to set
+
+    :return: None
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
